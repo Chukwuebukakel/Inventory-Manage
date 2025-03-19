a68b2f6f0ab8ae86ab6835bc7e4268e195cc8cba
Ebuka <buka2080@gmail.com>
2025-03-19 22:18:53 +0100

    third commit

--- IVManagement/settings.py
+++ IVManagement/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ipwfyny51!3vvoy-d3bn)4)=py%gqu=wf+bcix44-(9=8nv-52'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diabond-inventory-manage.onrender.com', 'Localhost', '127.0.0.1']
 
 
 # Application definition
